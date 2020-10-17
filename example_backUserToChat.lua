cd52db4f5e0c82a254de5b646cfc9d2cae7cd392
Irval <56792892+Irval1337@users.noreply.github.com>
2020-10-17 23:23:23 +0300

    Update example_backUserToChat.lua

--- example_backUserToChat.lua
+++ example_backUserToChat.lua
@@ -9,7 +9,7 @@ local label1 = UI:Label('Enter UserID', 11.0, 15, 20, panel)
 local textbox1 = UI:TextBox('Nickname or ID', 10.0, 200, 22, 15, 50, 15, panel)
 local label2 = UI:Label('Enter ChatID (digits only)', 11.0, 15, 105, panel)
 local textbox2 = UI:TextBox('ChatId', 10.0, 200, 22, 15, 135, 15, panel)
-local button = UI:Button('Back user to the chat', 165, 35, 40 + 34, 310, 'filled_2', 9.0, nil)
+local button = UI:Button('Back user to the chat', 165, 35, 74, 310, 'filled_2', 9.0, nil)
 
 local function onClick()
     local userid = textbox1.Text;
@@ -18,14 +18,14 @@ local function onClick()
 		UI:Alert('TextBox is empty', 'Error')
 	else
 		local response = VK:SendRequest('utils.resolveScreenName', '5.124', 'screen_name='..userid, nil)
-        local data = json.decode(response)
-        if data['type'] == 'user' then
-            local user = data['object_id']
-            VK:SendRequest('messages.removeChatUser', '5.21', 'chat_id='..chatid..'&user_id='..user, nil)
-            VK:SendRequest('messages.addChatUser', '5.124', 'chat_id='..chatid..'&user_id='..user, nil)
-        else
-            UI:Alert('Unsupported user type', 'Error')
-        end
+        	local data = json.decode(response)
+        	if data['type'] == 'user' then
+            		local user = data['object_id']
+            		VK:SendRequest('messages.removeChatUser', '5.21', 'chat_id='..chatid..'&user_id='..user, nil)
+            		VK:SendRequest('messages.addChatUser', '5.124', 'chat_id='..chatid..'&user_id='..user, nil)
+        	else
+            		UI:Alert('Unsupported user type', 'Error')
+        	end
 	end
 end
 

c07ad4e70182d27ff1a740c4bc948d6b756ba44c
Irval <56792892+Irval1337@users.noreply.github.com>
2020-10-18 21:05:56 +0300

    Add files via upload

---
+++ example_protectedOnClick.lua
@@ -0,0 +1,18 @@
+-- Пример скрипта, используещего безопасный вызов и установку метода button.OnClick
+
+UI:Initialize()
+local button = UI:Button('Resolve', 125, 29, 51, 95, 'filled_2', 9.0, nil)
+
+local function onClick()
+	UI:Message('Test', 'Message')
+end
+
+local function onClickProtected()
+    if pcall(onClick) then
+        UI:Alert('Successfully', 'Success')
+     else
+        UI:Alert('An error has occurred', 'Error')
+     end
+end
+
+UI:AddOnClick(button, onClickProtected())
